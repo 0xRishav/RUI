0c8ed1aca1c7efdb9c587b255008edaf248942e6
Rishav Bharti <rshvbharti@gmail.com>
2021-05-06 09:44:17 +0530

    added side nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./App.css";
+import { Home } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      App
+      <div className="app__wrapper">
+        <Home />
       </div>
+    </div>
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,23 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__sideNav">
+        <h4>RUI Components</h4>
+        <div className="home__sideNavLink">Avatar</div>
+        <div className="home__sideNavLink">Alerts</div>
+        <div className="home__sideNavLink">Badges</div>
+        <div className="home__sideNavLink">Buttons</div>
+        <div className="home__sideNavLink">Cards</div>
+        <div className="home__sideNavLink">Images</div>
+        <div className="home__sideNavLink">Input</div>
+        <div className="home__sideNavLink">Linkst</div>
+        <div className="home__sideNavLink">Navigation</div>
+        <div className="home__sideNavLink">Modal</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
