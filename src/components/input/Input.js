c59bb14d7f508a94ace927cc72c82ec4ce6d96e5
Rishav Bharti <rshvbharti@gmail.com>
2021-05-08 00:33:59 +0530

    made changes acc to reviews

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -3,7 +3,7 @@ import "./Input.css";
 
 function Input() {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <div className="input">
       <div class="form__group field">
         <input
           type="input"
